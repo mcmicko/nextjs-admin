c7d553f69d870b5c67903a9612432b485207e9d2
mcmicko <mcmzrc@gmail.com>
2024-04-09 19:37:19 +0200

    Dashboard Design 1

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import Card from '../ui/dashboard/card/card';
+import styles from '../ui/dashboard/dashboard.module.css';
+import Rightbar from '../ui/dashboard/rightbar/rightbar';
 
-function Dashboard() {
-  return <div>Dashboard</div>;
-}
+const page = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}></div>
 
-export default Dashboard;
+      <div className={styles.cards}>
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
+};
+
+export default page;
