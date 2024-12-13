2a099648ad469a526297767ab4ad89d2b1f61198
Milos Mandic <mickozr@gmail.com>
2024-12-13 20:28:14 +0100

    Server Action

--- app/dashboard/products/add/page.jsx
+++ app/dashboard/products/add/page.jsx
@@ -1,24 +1,25 @@
+import { addProduct } from '@/app/lib/actions';
 import styles from '@/app/ui/dashboard/products/addProduct/addProduct.module.css';
 
 const AddProductPage = () => {
   return (
     <div className={styles.container}>
-      <form action='' className={styles.form}>
-        <input type='text' placeholder='title' required />
+      <form action={addProduct} className={styles.form}>
+        <input type='text' placeholder='title' name='title' required />
         <select name='cat' id='cat'>
           <option value='general'>Choose a Category</option>
           <option value='kitchen'>Kitchen</option>
           <option value='phone'>Phone</option>
           <option value='computer'>Computer</option>
         </select>
-        <input type='number' placeholder='pice' name='price' />
-        <input type='number' placeholder='stock' name='stock' />
+        <input type='number' placeholder='price' name='price' required />
+        <input type='number' placeholder='stock' name='stock' required />
         <input type='text' placeholder='color' name='color' />
         <input type='text' placeholder='size' name='size' />
         <textarea
+          required
           name='desc'
           id='desc'
-          cols='30'
           rows='16'
           placeholder='Description'
         ></textarea>
--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -1,9 +1,10 @@
 import styles from '@/app/ui/dashboard/users/addUser/addUser.module.css';
+import { addUser } from '@/app/lib/actions';
 
 const AddUserPage = () => {
   return (
     <div className={styles.container}>
-      <form action='' className={styles.form}>
+      <form action={addUser} className={styles.form}>
         <input type='text' placeholder='username' name='username' required />
         <input type='email' placeholder='email' name='email' required />
         <input
@@ -14,17 +15,12 @@ const AddUserPage = () => {
         />
         <input type='phone' placeholder='phone' name='phone' required />
         <select name='isAdmin' id='isAdmin'>
-          <option value='false'>Is Admin</option>
-          <option value={false} selected>
-            Is Admin
-          </option>
+          <option value={false}>Is Admin</option>
           <option value={true}>Yes</option>
           <option value={false}>No</option>
         </select>
         <select name='isActive' id='isActive'>
-          <option value={true} selected>
-            Is Active?
-          </option>
+          <option value={true}>Is Active?</option>
           <option value={true}>Yes</option>
           <option value={false}>No</option>
         </select>
